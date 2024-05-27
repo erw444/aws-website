5c9528b4a81189a961dbaaa039e5446c72c0650a
Ethan Whittemore <erw444@gmail.com>
2024-05-27 10:26:02 -0500

    Add hardcoded api gateway path.

--- frontend/static/js/resources.js
+++ frontend/static/js/resources.js
@@ -3,8 +3,8 @@
   angular.module('app')
   .factory('Blogs', ['$resource', function($resource) {
     return $resource('/blog', {}, {
-      query: {method: 'GET', url: '/blog/all', isArray: true},
-      save: {method: 'POST', url: '/blog/new'}
+      query: {method: 'GET', url: 'https://22cnvs5mcc.execute-api.us-east-1.amazonaws.com/items', isArray: true},
+      save: {method: 'PUT', url: 'https://22cnvs5mcc.execute-api.us-east-1.amazonaws.com/items'}
     });
   }]);
 })();
